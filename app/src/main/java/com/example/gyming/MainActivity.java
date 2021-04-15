ddff3953f85a50c2119dab36232112090cbf2d2e
AlexM <muitinyau@gmail.com>
2021-04-15 15:18:38 +0800

    change layout

--- app/src/main/java/com/example/gyming/MainActivity.java
+++ app/src/main/java/com/example/gyming/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.gyming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        switch (v.getId()) {
+
+            case R.id.button: {
+                Intent i = new Intent(MainActivity.this, record.class);
+                startActivity(i);
+                break; }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/gyming/reflection.java
@@ -0,0 +1,14 @@
+package com.example.gyming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reflection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reflection);
+    }
+}
